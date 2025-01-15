4ceb13b63aada5cac38e1218b2d2367e057112f6
mamengchen <624718596@qq.com>
2025-01-15 23:05:48 +0800

    lua

--- demo/Luaxer/src/include/Interpreter.h
+++ demo/Luaxer/src/include/Interpreter.h
@@ -1,15 +1,24 @@
 #include "Parser.h"
+#include "VirtualStack.h"
+#include <memory>
 
 class Interpreter
 {
 private:
-
+    VirtualStack stack_;
 public:
-    Interpreter(/* args */){};
+    Interpreter(/* args */):stack_(){};
     ~Interpreter() {};
     
     void interpret(ASTNode* node) {
         if (!node) return;
     }
+
+    void interpretAdd(int a, int b) {
+        stack_.push(a);
+        stack_.push(b);
+    }
+
+    
 };
 
--- demo/Luaxer/src/include/VirtualStack.h
+++ demo/Luaxer/src/include/VirtualStack.h
@@ -8,3 +8,35 @@
 
 // 支持的栈数据类型
 using Value = std::variant<int, double, std::string>;
+class VirtualStack {
+public:
+    void push(const Value& value) {
+        stack_.push_back(value);
+    }
+
+    Value pop() {
+        if (stack_.empty()) {
+            throw std::runtime_error("Stack underflow");
+        }
+        Value top = stack_.back();
+        stack_.pop_back();
+        return top;
+    }
+
+    Value peek() const {
+        if (stack_.empty()) {
+            throw std::runtime_error("Stack is empty");
+        }
+        return stack_.back();
+    }
+
+    void printStack() const {
+        std::cout << "Stack (top -> bottom):" << std::endl;
+        for (auto it = stack_.rbegin(); it != stack_.rend(); ++it) {
+            std::visit([](auto&& value) { std::cout << value << std::endl; }, *it);
+        }
+    }
+
+private:
+    std::vector<Value> stack_;
+};
